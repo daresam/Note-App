806aff1ecfd066036012f67de775fa629864af9a
Dare Samuel <dawakesam@gmail.com>
2018-04-29 16:25:01 +0100

    Git Ignore File Added

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-console.log('Started app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
+const _ = require('lodash');
 
-user = os.userInfo();
+// console.log(_.isString(true));
+// console.log(_.isString('Drey'));
 
-fs.appendFile('message.txt', `Hello  ${user.username}`, (err) => {
-    if (err) 
-        throw err;
-    console.log('success');
-});
+const filteredArray = _.uniq([1,3,3,2,2,4,6,6]);
+console.log(filteredArray);
+
+// const res  = notes.add(2, 3);
+// console.log(res);
+
+// user = os.userInfo();
+
+// fs.appendFile('message.txt', `Hello  ${user.username}! You are ${notes.age}`, (err) => {
+//     if (err) 
+//         throw err;
+//     console.log('success');
+// });
---
+++ notes.js
@@ -0,0 +1,11 @@
+console.log('Starting notes.js');
+
+module.exports.addNote = (a, b) => {
+    const sum =  a + b;
+    return sum;
+};
+
+module.exports.add = (a, b) => {
+    const sum =  a + b;
+    return sum;
+};
